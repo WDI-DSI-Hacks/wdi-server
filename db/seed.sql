83122c949f283e0ac62cabc3371fc2efa2ceb87b
Jackie Casper <Casper.Jaclyn@gmail.com>
2017-04-06 17:44:06 -0400

    getting some data

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS profits;
+
+-- create users table
+CREATE TABLE profits(
+  id SERIAL PRIMARY KEY,
+  store INT NOT NULL,
+  dept INT NOT NULL,
+  week DATE NOT NULL,
+  weekly_sales FLOAT NOT NULL,
+  is_holiday BOOL NOT NULL
+);
+
+COPY profits(store,dept,week,weekly_sales,is_holiday)
+FROM '/Users/Jackie/code/wdi/projects/hackathon/wdi-server/db/train.csv' DELIMITER ',' CSV HEADER;
