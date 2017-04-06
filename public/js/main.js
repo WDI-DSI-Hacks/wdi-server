f2d5d5a03ef58af4cfc4d23b97a197d9f09a7095
Robert Romulus <rob@ovigz.com>
2017-04-06 17:49:51 -0400

    added chart.js related files

--- public/js/main.js
+++ public/js/main.js
@@ -36,11 +36,11 @@ window.onload = function() {
 
         data: [{
             type: "bubble",
-            toolTipContent: "<span style='\"'color: {color};'\"'><strong>{label}</strong></span><br/> <strong>Land Area</strong> {x} mn sq. km <br/> <strong>Rail Road</strong> {y} km<br/> <strong>Population</strong> {z} mn",
+            toolTipContent: "<span style='\"'color: {color};'\"'><strong>{label}</strong></span><br/> <strong>Revenue</strong> {x} Millions <br/> <strong>Profit</strong> {y} Millions <br/> <strong>Loss</strong> {z} Millions",
             dataPoints: [
                 { x: 9.14, y: 228513, z: 309.34, label: "Store1" },
                 { x: 16.37, y: 85292, z: 141.92, label: "Store2" },
-                { x: 1.21, y: 22051, z: 49.99, label: "Store3" },
+                { x: 1.21, y: 22051, z: 120.99, label: "Store3" },
                 { x: 9.327, y: 66239, z: 1337, label: "Store4" },
                 { x: 9.09, y: 58345, z: 34.12, label: "Store5" },
                 { x: 8.45, y: 29817, z: 194.94, label: "Store6" },
