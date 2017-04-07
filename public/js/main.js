a17e65f21a128762dd39113012e511341d4c99f6
Robert Romulus <rob@ovigz.com>
2017-04-07 11:10:51 -0400

    objects retrieve through ajax call

--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,60 @@
 
 console.log('Main JS working');
 $(document).ready(function() {
-// $.ajax({
-//   url: 'localhost:3000/api/stores/findeeps',
-//   dataType: 'json',
-//   data: results;
-// })
-// .done(function() {
-//   console.log("success");
-// })
-// .fail(function() {
-//   console.log("error");
-// })
-// .always(function() {
-//   console.log("complete");
-// });
+$.ajax({
+url: 'http://localhost:3000/api/stores/finddeps',
+type: 'GET',
+success: function(data){
+console.log(data);
+},
+
+error: function(data){
+console.log(data);
+}
+
+});
+
+});
+
+
+
 
 const CHART = document.getElementById('linechart');
 console.log(CHART);
-let lineChart = new Chart(CHART, {
-  type: 'line',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-           {
+// let lineChart = new Chart(CHART, {
+//   type: 'line',
+//   data: {
+//     labels: ["January", "February", "March", "April", "May", "June", "July"],
+//     datasets: [
+//            {
 
-            label: "My First dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [65, 59, 80, 81, 56, 55, 40],
+//             label: "My First dataset",
+//             fill: false,
+//             lineTension: 0.1,
+//             backgroundColor: "rgba(75,192,192,0.4)",
+//             borderColor: "rgba(75,192,192,1)",
+//             borderCapStyle: 'butt',
+//             borderDash: [],
+//             borderDashOffset: 0.0,
+//             borderJoinStyle: 'miter',
+//             pointBorderColor: "rgba(75,192,192,1)",
+//             pointBackgroundColor: "#fff",
+//             pointBorderWidth: 1,
+//             pointHoverRadius: 5,
+//             pointHoverBackgroundColor: "rgba(75,192,192,1)",
+//             pointHoverBorderColor: "rgba(220,220,220,1)",
+//             pointHoverBorderWidth: 2,
+//             pointRadius: 1,
+//             pointHitRadius: 10,
+//             data: [65, 59, 80, 81, 56, 55, 40],
 
-            }
-         ]
-}
-})
- });
+//             }
+//          ]
+// }
+// })
+
+
+
 
 
 
